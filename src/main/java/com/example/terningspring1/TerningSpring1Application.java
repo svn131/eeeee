71c41976aeb2de81c0aef5245a1856404cd27642
Vladimir Solovyov <svn131131@gmail.com>
2023-10-06 19:45:34 +0500

    Initial commit

---
+++ src/main/java/com/example/terningspring1/TerningSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.terningspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerningSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerningSpring1Application.class, args);
+	}
+
+}
